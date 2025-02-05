c1a5b427bbd1adfedb42ba2ac4847993a8ce08a7
Bjoern <bjoern.c3@nixda.biz>
2025-02-05 10:20:29 +0100

    add cmdline args to watchdog

--- Software/bjoern/i2c-watchdog.c
+++ Software/bjoern/i2c-watchdog.c
@@ -60,22 +60,45 @@ static inline __s32 i2c_smbus_write_byte_data(int file, __u8 command, __u8 value
 				I2C_SMBUS_BYTE_DATA, &data);
 }
 
+
 int main(int argc, char **argv)
 {
-	uint8_t data, addr = 0x08, reg = 14, fw_id, fw_version;
-	const char *path = argv[1];
-	int fd, rc;
+	uint8_t data, addr = 0x08, fw_id, fw_version;
+	char *path = "/dev/i2c-0";
+	char *shutdown_command = "/usr/sbin/poweroff";
+	unsigned int check_intervall_ms = 500;
+	int fd, rc, opt;
 
-	if (argc == 1)
-		errx(-1, "path [i2c address]");
 
-	if (argc > 2)
-		addr = strtoul(argv[2], NULL, 0);
+	opterr = 0;
+	while ((opt = getopt (argc, argv, "i:a:c:b:")) != -1){
+		switch (opt) {
+		case 'c':
+			shutdown_command = optarg;
+			break;
+		case 'b':
+			path = optarg;
+			break;
+		case 'a':
+			//errx(EXIT_FAILURE,"%c not implemented...\n",opt);
+			addr = strtoul(optarg, NULL, 0);
+			break;
+		case 'i':
+			check_intervall_ms = atoi(optarg);
+			if (!check_intervall_ms){
+				errx(EXIT_FAILURE, "check intervall invalid");
+			}
+			break;
+		default: /* '?' */
+			fprintf(stderr, "Usage: %s [-i msecs(default %i)] [-a i2c-address (default 0x%02x)] [-b %s] [-c shutdown-command ]\n", argv[0], check_intervall_ms, addr, path);
+			exit(EXIT_FAILURE);
+		}
+	}
 
 
 	fd = open(path, O_RDWR);
 	if (fd < 0)
-		err(errno, "Tried to open '%s'", path); 
+		err(errno, "Tried to open '%s'", path);
 
 	rc = ioctl(fd, I2C_SLAVE, addr);
 	if (rc < 0)
@@ -89,34 +112,40 @@ int main(int argc, char **argv)
 	fw_version = data;
 
 	if (fw_id == 0x37 && fw_version > 0x80){
-		printf("WittyPi with watchdog rev %02x found\n", fw_version);
+		printf("WittyPi with watchdog rev 0x%02x found\n", fw_version);
 
 		printf("sending SysUp...\n");
 		if (i2c_smbus_write_byte_data(fd, 14, 1)<0)
 			err(errno, "Write error '%s' addr '0x%02x' reg %d\n", path, addr, 14);
 
-		//TODO signal sysUP
 		//TODO switch on watchdog
-		//TODO poll reg 14 check for bit 3 -> run shutdown when set
 		//TODO catch ctrl+c disable watchdog on exit
 		while(1){
-			if (-1 == (data = i2c_smbus_read_byte_data(fd, 14))) // 14  // flags & trigger & watchdog | write bit: 0:SYS_UP 1:RESET 2:PWR_BTN 3:wdt_on 4:wdt_off 5: 6: 7:  | read bit: 0:SYSisUP 1:WDTon? 2:turningOff? 3:
+			if (-1 == (data = i2c_smbus_read_byte_data(fd, 14))) // 14  // flags & trigger & watchdog | read bit: 0:SYSisUP 1:WDTon? 2:turningOff? 3: 4: test-toggle
 				err(errno, "Read error '%s' addr '0x%02x'\n", path, addr);
 
-			if (data & (1<<4)){
+			if (data & (1<<4)){ //test mode
 				printf("data 0x%02x -> bit 4 set !!\n", data);
+				char test_shutdown_command[1024];
+				snprintf(test_shutdown_command, sizeof(test_shutdown_command), "echo %s",shutdown_command);
+				printf("exec '%s'\n", test_shutdown_command);
+				system(test_shutdown_command);
 			}
 
 			if (data & 1<<2) { // check for TurningOff
 				printf("turningOff bit set in WittyPi reg 14! Shutting down System...\n");
-				system("/usr/sbin/shutdown");
+				printf("exec '%s'\n", shutdown_command);
+				system(shutdown_command);
+				break;
 			}
 
-			//sleep(1);
-			usleep(500*1000);
+			usleep(check_intervall_ms * 1000);
 		}
 		//data = i2c_smbus_read_byte_data(fd, reg);
 		//printf("%s: device 0x%02x at address 0x%02x: 0x%02x\n",	path, addr, reg, data);
+	}else{
+		err(1,"fw_id[0x%02x] != 0x37 or fw_version[0x%02x] < 0x80", fw_id, fw_version);
 	}
 
+	return 0;
 }
